1a05eb6ea0ed5898fe99d70ff9b944a68a61b74f
dnguye05 <77364532+dnguye05@users.noreply.github.com>
2021-10-18 21:38:52 -0500

    2021-10-28 upload files

--- project2.cob
+++ project2.cob
@@ -33,7 +33,9 @@
            03 I-EMPTYSPACES1 PIC X(24).
            03 I-DATE PIC X(8).
            03 I-EMPTYSPACES2 PIC X(2).
-           03 I-EMPRATE PIC 9(6).
+           03 I-EMPRATE.
+               05 I-EMPRATEWHOLE PIC 9(4).
+               05 I-EMPRATEDECIMAL PIC P9(2).
            03 I-EMPSTATUS PIC X(1).
        01  PRNT-DATA1.
            03 L-SSN.
@@ -48,7 +50,7 @@
                05 L-FORMATDATE PIC 99/99/9999.
                05 FILLER PIC X(5) VALUES SPACES.
            03 L-EMPRATE.
-               05 L-FORMATEEMPRATE PIC 9999B99.
+               05 L-FORMATEEMPRATE PIC ZZZZ.99.
                05 FILLER PIC X(3) VALUES SPACES.
            03 L-EMPSTATUS PIC X(1).
        01  PRNT-HEADING1.
@@ -75,57 +77,71 @@
        01  PRNT-FOOTER1.
            03 FILLER PIC X(40) VALUES 
            'NUMBER OF EMPLOYEE RECORDS READ:'.
-           03 F1-EMPCOUNTER PIC 9(4) VALUE ZERO.
+           03 F1-EMPCOUNTER PIC ZZZ9.
        01  PRNT-FOOTER2.
            03 FILLER PIC X(40) VALUES 'NUMBER OF HOURLY EMPLOYEES:'.
-           03 F2-HEMPCOUNT PIC 9(4) VALUE ZERO.
+           03 F2-HEMPCOUNT PIC ZZZ9.
            03 FILLER PIC X(5).
            03 FILLER PIC X(40) VALUES 'AVERAGE HOURLY RATE:'.
-           03 F2-AVGHRATE PIC $999.99.
+           03 F2-AVGHRATE PIC $ZZZ.99.
        01  PRNT-FOOTER3.
            03 FILLER PIC X(40) VALUES 'NUMBER OF SALARIED EMPLOYEES:'.
-           03 F3-SEMPCOUNT PIC 9(4) VALUE ZERO.
+           03 F3-SEMPCOUNT PIC ZZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(40) VALUES 'AVERAGE SALARIED RATE:'.
-           03 F3-AVGSRATE PIC $9999.99.
+           03 FILLER PIC X(38) VALUES 'AVERAGE SALARIED RATE:'.
+           03 F3-AVGSRATE PIC $Z,ZZZ.99.
        01  PRNT-FOOTER4.
-           03 FILLER PIC X(10) VALUES 'TYPE 1:'.
-           03 F4-T1 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 1:'.
+           03 F4-T1 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 2:'.
-           03 F4-T2 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 2:'.
+           03 F4-T2 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 3:'.
-           03 F4-T3 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 3:'.
+           03 F4-T3 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 4:'.
-           03 F4-T4 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 4:'.
+           03 F4-T4 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 5:'.
-           03 F4-T5 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 5:'.
+           03 F4-T5 PIC ZZ9.
            03 FILLER PIC X(5).
        01  PRNT-FOOTER5.
-           03 FILLER PIC X(10) VALUES 'TYPE 6:'.
-           03 F5-T6 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 6:'.
+           03 F5-T6 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 7:'.
-           03 F5-T7 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 7:'.
+           03 F5-T7 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 8:'.
-           03 F5-T8 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 8:'.
+           03 F5-T8 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 9:'.
-           03 F5-T9 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 9:'.
+           03 F5-T9 PIC ZZ9.
            03 FILLER PIC X(5).
-           03 FILLER PIC X(10) VALUES 'TYPE 10:'.
-           03 F5-T10 PIC 9(3).
+           03 FILLER PIC X(12) VALUES 'TYPE 10:'.
+           03 F5-T10 PIC ZZ9.
            03 FILLER PIC X(5).
        01  MISC.
            03 EOF-I PIC 9 VALUE 0.
            03 PGNUM PIC 999 VALUE 1.
            03 RECORDPAGECOUNTER PIC 99 VALUE 0.
-           03 TOTALHRATE PIC 9(12).
-           03 TOTALSRATE PIC 9(12).
+           03 EMPCOUNTER PIC 9(4).
+           03 EMPHCOUNT PIC 9(4).
+           03 EMPSCOUNT PIC 9(4).
+           03 TOTALHRATE PIC 9(8)V9(2).
+           03 TOTALSRATE PIC 9(10)V9(2).
+           03 EMPRATE-FORMATER PIC 9(4)V9(2).
+           03 T1 PIC 9(3).
+           03 T2 PIC 9(3).
+           03 T3 PIC 9(3).
+           03 T4 PIC 9(3).
+           03 T5 PIC 9(3).
+           03 T6 PIC 9(3).
+           03 T7 PIC 9(3).
+           03 T8 PIC 9(3).
+           03 T9 PIC 9(3).
+           03 T10 PIC 9(3).
        PROCEDURE DIVISION.
        000-MAINLINE.
            OPEN INPUT INPUT-FILE
@@ -146,10 +162,10 @@
                ELSE
                    MOVE SPACES TO PRNT-REC
                    WRITE PRNT-REC
-                       AFTER ADVANCING 1 LINE
+                       AFTER ADVANCING PAGE
                    MOVE SPACES TO PRNT-REC
                    WRITE PRNT-REC
-                       AFTER ADVANCING PAGE
+                       AFTER ADVANCING 1 LINE
                    WRITE PRNT-REC FROM PRNT-HEADING1
                        AFTER ADVANCING 1 LINE
            END-IF.
@@ -196,63 +212,79 @@
            MOVE I-TITLE TO L-TITLE.
            MOVE I-EMPTYPE TO L-EMPTYPE.
            IF I-EMPTYPE = '01'
-              COMPUTE F4-T1 = F4-T1 + 1
+              COMPUTE T1 = T1 + 1
            END-IF.
            IF I-EMPTYPE = '02'
-              COMPUTE F4-T2 = F4-T2 + 1
+              COMPUTE T2 = T2 + 1
            END-IF.
            IF I-EMPTYPE = '03'
-              COMPUTE F4-T3 = F4-T3 + 1
+              COMPUTE T3 = T3 + 1
            END-IF.
            IF I-EMPTYPE = '04'
-              COMPUTE F4-T4 = F4-T4 + 1
+              COMPUTE T4 = T4 + 1
            END-IF.
            IF I-EMPTYPE = '05'
-              COMPUTE F4-T5 = F4-T5 + 1
+              COMPUTE T5 = T5 + 1
            END-IF.
            IF I-EMPTYPE = '06'
-              COMPUTE F5-T6 = F5-T6 + 1
+              COMPUTE T6 = T6 + 1
            END-IF.
            IF I-EMPTYPE = '07'
-              COMPUTE F5-T7 = F5-T7 + 1
+              COMPUTE T7 = T7 + 1
            END-IF.
            IF I-EMPTYPE = '08'
-              COMPUTE F5-T8 = F5-T8 + 1
+              COMPUTE T8 = T8 + 1
            END-IF.
            IF I-EMPTYPE = '09'
-              COMPUTE F5-T9 = F5-T9 + 1
+              COMPUTE T9 = T9 + 1
            END-IF.
            IF I-EMPTYPE = '10'
-              COMPUTE F5-T10 = F5-T10 + 1
+              COMPUTE T10 = T10 + 1
            END-IF.
            MOVE I-DATE TO L-FORMATDATE.
-           MOVE I-EMPRATE TO L-FORMATEEMPRATE.
-           INSPECT L-FORMATEEMPRATE REPLACING ALL ' ' BY '.'.
+           MOVE I-EMPRATE TO EMPRATE-FORMATER.
+           MOVE EMPRATE-FORMATER TO L-FORMATEEMPRATE.
            MOVE I-EMPSTATUS TO L-EMPSTATUS.
            IF L-EMPSTATUS = 'H'
-               COMPUTE F2-HEMPCOUNT = F2-HEMPCOUNT + 1
-               COMPUTE TOTALHRATE = TOTALHRATE + I-EMPRATE
+               COMPUTE EMPHCOUNT = EMPHCOUNT + 1
+               COMPUTE TOTALHRATE = TOTALHRATE + EMPRATE-FORMATER
               ELSE
-               COMPUTE F3-SEMPCOUNT = F3-SEMPCOUNT + 1
-               COMPUTE TOTALSRATE = TOTALSRATE + I-EMPRATE
+               COMPUTE EMPSCOUNT = EMPSCOUNT + 1
+               COMPUTE TOTALSRATE = TOTALSRATE + EMPRATE-FORMATER
            END-IF.
            WRITE PRNT-REC FROM PRNT-DATA1
                AFTER ADVANCING 1 LINE.
            COMPUTE RECORDPAGECOUNTER = RECORDPAGECOUNTER + 1.
-           COMPUTE F1-EMPCOUNTER = F1-EMPCOUNTER + 1.
+           COMPUTE EMPCOUNTER = EMPCOUNTER + 1.
        1700-PRINT-FOOTER.
-           COMPUTE TOTALHRATE = TOTALHRATE / F2-HEMPCOUNT.
-           COMPUTE TOTALSRATE = TOTALSRATE / F3-SEMPCOUNT.
-           MOVE TOTALHRATE TO F2-AVGHRATE.
-           MOVE TOTALSRATE TO F3-AVGSRATE.
+           MOVE EMPCOUNTER TO F1-EMPCOUNTER.
            WRITE PRNT-REC FROM PRNT-FOOTER1
                AFTER ADVANCING 1 LINE.
+           MOVE EMPHCOUNT TO F2-HEMPCOUNT.
+           COMPUTE TOTALHRATE = TOTALHRATE / EMPHCOUNT.
+           MOVE TOTALHRATE TO F2-AVGHRATE.
            WRITE PRNT-REC FROM PRNT-FOOTER2
                AFTER ADVANCING 1 LINE.
+           MOVE EMPSCOUNT TO F3-SEMPCOUNT
+           COMPUTE TOTALSRATE = TOTALSRATE / EMPSCOUNT.
+           MOVE TOTALSRATE TO F3-AVGSRATE.
            WRITE PRNT-REC FROM PRNT-FOOTER3
                AFTER ADVANCING 1 LINE.
+           MOVE SPACES TO PRNT-REC.
+           WRITE PRNT-REC
+               AFTER ADVANCING 1 LINE.
+           MOVE T1 TO F4-T1.
+           MOVE T2 TO F4-T2.
+           MOVE T3 TO F4-T3.
+           MOVE T4 TO F4-T4.
+           MOVE T5 TO F4-T5.
            WRITE PRNT-REC FROM PRNT-FOOTER4
                AFTER ADVANCING 1 LINE.
+           MOVE T6 TO F5-T6.
+           MOVE T7 TO F5-T7.
+           MOVE T8 TO F5-T8.
+           MOVE T9 TO F5-T9.
+           MOVE T10 TO F5-T10.
            WRITE PRNT-REC FROM PRNT-FOOTER5
                AFTER ADVANCING 1 LINE.
        2000-READ-INPUT.
